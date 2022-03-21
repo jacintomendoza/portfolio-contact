2cc7f969c7573482901ab0915cdc0b240e67e37d
Josh Mendoza <jjm121197@gmail.com>
2022-03-21 12:20:18 -0500

    remove string returns

--- src/main/java/com/example/portfoliocontact/controller/contactController.java
+++ src/main/java/com/example/portfoliocontact/controller/contactController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@ResponseBody
 @CrossOrigin
 @RestController
 public class contactController {
@@ -21,9 +20,9 @@ public class contactController {
     }
 
     @PostMapping(value="/addContact")
-    public String saveContact(@RequestBody Contact contact){
+    public void saveContact(@RequestBody Contact contact){
         contactRepo.save(contact);
-        return "Saved...";
+//        return "Saved...";
     }
 
     @GetMapping(value = "/contacts")
@@ -32,20 +31,20 @@ public class contactController {
     }
 
     @PutMapping(value = "/update/{id}")
-    public String updateContact(@PathVariable long id, @RequestBody Contact contact){
+    public void updateContact(@PathVariable long id, @RequestBody Contact contact){
         Contact updatedContact = contactRepo.findById(id).get();
         updatedContact.setFullName(contact.getFullName());
         updatedContact.setEmail(contact.getEmail());
         updatedContact.setComment(contact.getComment());
         contactRepo.save(updatedContact);
-        return "Updated...";
+//        return "Updated...";
     }
 
     @DeleteMapping(value = "/delete/{id}")
-    public String deleteContact(@PathVariable long id){
+    public void deleteContact(@PathVariable long id){
         Contact deleteContact = contactRepo.findById(id).get();
         contactRepo.delete(deleteContact);
-        return "Deleted contact with the id: "+id;
+//        return "Deleted contact with the id: "+id;
     }
 
 }
